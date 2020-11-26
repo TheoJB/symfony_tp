c7c1266e71c7e3d3d812ddcccf25074422b0ed73
Theo Jean-Baptiste <tjbaptiste@iutbayonne.univ-pau.fr>
2020-11-26 17:55:35 +0100

    Ajout des routes des pages entreprises, formations et stages/ (Fin TP1)

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -8,13 +8,32 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProStagesController extends AbstractController
 {
-    /**
-     * @Route("/", name="pro_stages_accueil")
-     */
     public function index(): Response
     {
         return $this->render('pro_stages/index.html.twig', [
             'controller_name' => 'ProStagesController',
         ]);
     }
+
+    public function entreprises(): Response
+  {
+      return $this->render('pro_stages/index.html.twig', [
+          'controller_name' => 'Bienvenue sur la page daccueil de Prostages',
+      ]);
+  }
+
+  public function formations(): Response
+{
+    return $this->render('pro_stages/index.html.twig', [
+        'controller_name' => 'Cette page affichera la liste des entreprises proposant un stage',
+    ]);
+}
+
+  public function stages($id): Response
+{
+    return $this->render('pro_stages/index.html.twig', [
+        'controller_name' => 'Cette page affichera le descriptif du stage ayant pour identifiant '.$id ,
+  ]);
+}
+
 }
