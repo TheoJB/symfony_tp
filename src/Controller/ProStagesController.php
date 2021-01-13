ad45d4b364779cf5e8496356e85e0824a76fa16e
Theo Jean-Baptiste <tjbaptiste@iutbayonne.univ-pau.fr>
2021-01-13 13:57:00 +0100

    Affichage des stages sur le site, accueil, triés par entreprises et triés par formations

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -5,34 +5,55 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Stage;
+use App\Entity\Entreprise;
+use App\Entity\Formation;
+
 
 class ProStagesController extends AbstractController
 {
     public function index(): Response
     {
-        return $this->render('pro_stages/index.html.twig', [
-          
-        ]);
+      //Recupérer le repository de l'entité stage
+      $repositoryStage = $this->getDoctrine()->getRepository(stage::class);
+
+      //Récupérer les stages enregistrées en BD
+      $stages = $repositoryStage->findAll();
+
+      //Envoyer les stages à la vue chargée de les afficher
+        return $this->render('pro_stages/index.html.twig', ['stages'=>$stages]);
     }
 
     public function entreprise(): Response
   {
-      return $this->render('pro_stages/entreprise.html.twig', [
+    $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
 
+    //Récupérer les stages enregistrées en BD
+    $entreprises = $repositoryEntreprise->findAll();
+
+      return $this->render('pro_stages/entreprise.html.twig', [
+        'entreprises'=>$entreprises,
       ]);
   }
 
   public function formation(): Response
 {
-    return $this->render('pro_stages/formation.html.twig', [
+    $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
+    $formations = $repositoryFormation->findAll();
 
+    return $this->render('pro_stages/formation.html.twig', [
+        'formations'=>$formations
     ]);
 }
 
   public function stage($id): Response
-{
+{  //Recupérer le repository de l'entité stage
+      $repositoryStage = $this->getDoctrine()->getRepository(stage::class);
+
+      //Récupérer les stages enregistrées en BD
+      $stage = $repositoryStage->find($id);
     return $this->render('pro_stages/stage.html.twig', [
-        'id' => $id,
+        'id' => $id, 'stage'=>$stage
   ]);
 }
 
